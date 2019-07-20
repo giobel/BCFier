ea5192dbb0b8461690a1b3e663ddee01f75ad114
GLOBAL\Giovanni.Brogiolo <giovanni.brogiolo>
2019-07-20 20:54:36 +1000

    fixes

--- Bcfier.Revit/Entry/CmdBatchIssues.cs
+++ Bcfier.Revit/Entry/CmdBatchIssues.cs
@@ -1,10 +1,12 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using Bcfier.Bcf;
 using Bcfier.Bcf.Bcf2;
 using Bcfier.Data.Utils;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -31,58 +33,80 @@ namespace Bcfier.Revit.Entry
       {
                 UIApplication uiapp = commandData.Application;
                 UIDocument uidoc = uiapp.ActiveUIDocument;
+                Document doc = uidoc.Document;
 
                 BcfContainer bcfContainer = new BcfContainer();
                 BcfFile bcfFile = new BcfFile();
 
-                Markup issue = new Markup(DateTime.Now);
+                IList<Reference> selectedIssues = uidoc.Selection.PickObjects(ObjectType.Element, "Select issues");
 
-                Topic tp = new Topic();
-                tp.Title = "My first Issue";
-                tp.Description = "a box and nothing else";
-                issue.Topic = tp;
-
-                
-
-                var view = new ViewPoint(issue.Viewpoints.Any());
-
-                if (!Directory.Exists(Path.Combine(bcfFile.TempPath, issue.Topic.Guid)))
-                    Directory.CreateDirectory(Path.Combine(bcfFile.TempPath, issue.Topic.Guid));
-
-
-
-                var c = new Comment
+                using (Transaction t = new Transaction(doc, "Export selected issues"))
                 {
-                    Comment1 = "Comment something",
-                    Author = Utils.GetUsername(),
-                    //Status = comboStatuses.SelectedValue.ToString(),
-                    //VerbalStatus = VerbalStatus.Text,
-                    Date = DateTime.Now,
-                    Viewpoint = new CommentViewpoint { Guid = view.Guid }
-                    };
-                    issue.Comment.Add(c);
+                    t.Start();
+
+                    foreach (Reference item in selectedIssues)
+                    {
+
+
+                        Element issueElement = doc.GetElement(item);
+
+                        Markup issue = new Markup(DateTime.Now);
+
+                        Topic tp = new Topic();
+                        tp.Title = issueElement.Name;
+                        tp.Description = issueElement.Id.ToString();
+                        issue.Topic = tp;
+
+
+
+                        var view = new ViewPoint(issue.Viewpoints.Any());
+
+                        if (!Directory.Exists(Path.Combine(bcfFile.TempPath, issue.Topic.Guid)))
+                            Directory.CreateDirectory(Path.Combine(bcfFile.TempPath, issue.Topic.Guid));
+
+
+
+                        var c = new Comment
+                        {
+                            Comment1 = "Comment something",
+                            Author = Utils.GetUsername(),
+                            //Status = comboStatuses.SelectedValue.ToString(),
+                            //VerbalStatus = VerbalStatus.Text,
+                            Date = DateTime.Now,
+                            Viewpoint = new CommentViewpoint { Guid = view.Guid }
+                        };
+                        issue.Comment.Add(c);
+
+                        uidoc.ActiveView.IsolateElementTemporary(item.ElementId);
+
+                        uidoc.ShowElements(item.ElementId);
+
+                        uidoc.ActiveView.DisableTemporaryViewMode(TemporaryViewMode.TemporaryHideIsolate);
+
+                        string path = Path.Combine(bcfFile.TempPath, issue.Topic.Guid, view.Snapshot);
+                        SaveRevitSnapshot(uidoc.Document, path);
+                        view.SnapshotPath = path;
+
+                        issue.Viewpoints.Add(view);
+
+                        issue.Viewpoints.Last().VisInfo = Data.RevitView.GenerateViewpoint(uiapp.ActiveUIDocument);
+
+                        if (uidoc.Document.Title != null)
+                            issue.Header[0].Filename = uidoc.Document.Title;
+                        else
+                            issue.Header[0].Filename = "Unknown";
+
+
+                        bcfFile.Issues.Add(issue);
+
+
+                    }
+
+                    t.Commit();
                 
+                 
+                }
 
-
-
-
-                //first save the image, then update path
-                string path = Path.Combine(bcfFile.TempPath, issue.Topic.Guid, view.Snapshot);
-                SaveRevitSnapshot(uidoc.Document, path);
-                view.SnapshotPath = path;
-
-                //neede for UI binding
-                issue.Viewpoints.Add(view);
-
-                issue.Viewpoints.Last().VisInfo = Data.RevitView.GenerateViewpoint(uiapp.ActiveUIDocument);
-
-                if (uidoc.Document.Title != null)
-                    issue.Header[0].Filename = uidoc.Document.Title;
-                else
-                    issue.Header[0].Filename = "Unknown";
-
-
-                bcfFile.Issues.Add(issue);
                 bcfContainer.SaveFile(bcfFile);
 
 
